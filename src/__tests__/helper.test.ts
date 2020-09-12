776fd0379e2100875439fc1dcf90e26145596a25
Yannick Fricke <yannickfricke@googlemail.com>
2020-09-12 20:54:51 +0200

    test(helper): added the "helper" tests
    Added tests for the following functions:
    - getOrDefault
    - hasLocalStorageValue
    - serializeValue
    - deserializeValue

---
+++ src/__tests__/helper.test.ts
@@ -0,0 +1,74 @@
+import {
+    deserializeValue,
+    getOrDefault,
+    hasLocalStorageValue,
+    serializeValue,
+} from '../helper';
+
+describe('Helper', () => {
+    const testKey = 'testing';
+    const defaultValue = {
+        test: true,
+    };
+
+    describe('getOrDefault', () => {
+        beforeEach(() => {
+            window.localStorage.removeItem(testKey);
+        });
+
+        it('should return the parsed value when the value is defined', () => {
+            window.localStorage.setItem(testKey, serializeValue(defaultValue));
+
+            const value = getOrDefault<typeof defaultValue>(testKey, {
+                test: false,
+            });
+
+            expect(value.test).toBeTruthy();
+        });
+
+        it('should return the default value when no value is defined', () => {
+            const value = getOrDefault<typeof defaultValue>(
+                testKey,
+                defaultValue,
+            );
+
+            expect(value).toBe(defaultValue);
+        });
+    });
+
+    describe('hasLocalStorageValue', () => {
+        beforeEach(() => {
+            window.localStorage.removeItem(testKey);
+        });
+
+        it('should return false when the value is null', () => {
+            expect(hasLocalStorageValue(testKey)).toBeFalsy();
+        });
+
+        it('should return true when the value is defined', () => {
+            window.localStorage.setItem(testKey, '');
+
+            expect(hasLocalStorageValue(testKey)).toBeTruthy();
+        });
+    });
+
+    describe('serializeValue', () => {
+        it('should invoke JSON.stringify', () => {
+            JSON.stringify = jest.fn();
+
+            expect(JSON.stringify).not.toHaveBeenCalled();
+            serializeValue('test');
+            expect(JSON.stringify).toHaveBeenCalled();
+        });
+    });
+
+    describe('deserializeValue', () => {
+        it('should invoke JSON.parse', () => {
+            JSON.parse = jest.fn();
+
+            expect(JSON.parse).not.toHaveBeenCalled();
+            deserializeValue('test');
+            expect(JSON.parse).toHaveBeenCalled();
+        });
+    });
+});
