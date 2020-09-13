e4afa14a3f2defc311dac7dd700a81d60ca247ed
Yannick Fricke <yannickfricke@googlemail.com>
2020-09-13 02:45:13 +0200

    ci(actions): added the codecov step to the "tests" job

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     rootDir: './src',
+    collectCoverage: true,
+    coverageReporters: ['lcov'],
+    coverageDirectory: '../coverage',
 };
